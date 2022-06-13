e4cfefb77a3d048d6b0250b8e1f9ab011e2c9380
QuangLyHo <lyhoqq@gmail.com>
2022-06-13 14:36:22 -0400

    Initialized project files

---
+++ index.js
@@ -0,0 +1,54 @@
+// const slides = document.getElementsByClassName('carousel-item')
+// let slidePosition = 0
+// const totalSlides = slides.length
+
+// document.getElementById('carousel-prev-btn').addEventListener('click', prevSlide)
+// document.getElementById('carousel-next-btn').addEventListener('click', nextSlide)
+
+
+// function showSlide(n) {
+//     hideAllSlides()
+//     slides[n].classList.add('carousel-item-visible')
+// }
+
+// function nextSlide() {
+//     hideAllSlides()
+
+//     // if (slidePosition === totalSlides - 1) {
+//     //     slidePosition = 0
+//     // } else slidePosition++
+
+//     slidePosition === totalSlides - 1 ?  slidePosition = 0 : slidePosition++
+
+//     showSlide(slidePosition)
+// }
+
+// function prevSlide() {
+//     hideAllSlides()
+    
+//     if (slidePosition === 0) {
+//         slidePosition = totalSlides - 1
+//     } else slidePosition--
+
+//     showSlide(slidePosition)
+// }
+
+// function hideAllSlides() {
+//     for (let slide of slides) {
+//         slide.classList.remove('carousel-item-visible')
+//         slide.classList.add('carousel-item-hidden')
+//     }
+// }
+
+
+// let autoIncrSlide = setInterval(nextSlide, 3000)
+
+function doubleloop() {
+    for (let i = 1; i < 5; i = i * 2) {
+        for (let j = 20; j > 16; j = j - 2) {
+            console.log(i + j)
+        }
+    }
+}
+
+doubleloop
